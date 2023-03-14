a9da67de919ef8fa6667cdb87db2e2bd207d28d0
abbydumke <111912949+abbydumke@users.noreply.github.com>
2023-03-14 14:42:14 -0400

    builder and geometry builder

--- src/input/builder/GeometryBuilder.java
+++ src/input/builder/GeometryBuilder.java
@@ -1,5 +1,47 @@
 package input.builder;
 
-public class GeometryBuilder {
+import java.util.List;
 
+import input.components.FigureNode;
+import input.components.point.PointNode;
+import input.components.point.PointNodeDatabase;
+import input.components.segment.SegmentNode;
+import input.components.segment.SegmentNodeDatabase;
+
+public class GeometryBuilder extends DefaultBuilder{
+
+	public GeometryBuilder() {
+	}
+	
+	 public FigureNode buildFigureNode(String description, 
+			 PointNodeDatabase points, SegmentNodeDatabase segments)
+	 {
+		 return new FigureNode(description,points, segments);
+	 }
+
+	 public SegmentNodeDatabase buildSegmentNodeDatabase()
+	 {
+		 return new SegmentNodeDatabase();
+	 }
+
+	 public void addSegmentToDatabase(SegmentNodeDatabase segments, PointNode from, PointNode to)
+	 {
+		 if (segments != null) segments.addUndirectedEdge(from, to);
+	 }
+
+	 public SegmentNode buildSegmentNode(PointNode pt1, PointNode pt2)
+	 {
+		 return new SegmentNode(pt1, pt2);
+	 }
+
+	 public PointNodeDatabase buildPointDatabaseNode(List<PointNode> points)
+	 {
+		 return new PointNodeDatabase(points);
+	 }
+
+	 public PointNode buildPointNode(String name, double x, double y)
+	 {
+		 return new PointNode(name, x, y);
+	 }
+	 
 }
