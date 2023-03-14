523b51ffcb5778e1407271c7297b434c0dd97ce6
SeanRowland7 <seanwrowland@gmail.com>
2023-03-14 15:35:54 -0400

    Added a test case for JSONParserTest
    not passing

--- test/input/parser/JSONParserTest.java
+++ test/input/parser/JSONParserTest.java
@@ -4,11 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import input.builder.DefaultBuilder;
+import input.components.ComponentNode;
+import input.components.FigureNode;
+
 class JSONParserTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	
+	public static ComponentNode runFigureParseTest(String filename)
+	{
+		JSONParser parser = new JSONParser(new DefaultBuilder());
 
+		String figureStr = utilities.io.FileUtilities.readFileFilterComments(filename);
+		
+		return parser.parse(figureStr);
+	}
+	
+	@Test
+	void colinear_line_segments_test()
+	{
+		
+		ComponentNode node = JSONParserTest.runFigureParseTest("collinear_line_segments.json");
+
+		assertTrue(node instanceof FigureNode);
+		
+		StringBuilder sb = new StringBuilder();
+		node.unparse(sb, 0);
+		System.out.println(sb.toString());
+	}
 }
